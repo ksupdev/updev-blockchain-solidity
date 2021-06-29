80347b552b91e00fa8e7e9a590eef10ec71519b1
ksupdev <41143926+ksupdev@users.noreply.github.com>
2021-06-30 02:18:14 +0700

    Update Contract.sol
    validate with require statement . function only get executed one time per user

--- Contract.sol
+++ Contract.sol
@@ -21,7 +21,6 @@ contract ZombieFactory {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
-
         emit NewZombie(id, _name, _dna);
     }
 
@@ -31,6 +30,7 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
