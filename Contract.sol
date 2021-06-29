4dd4c2abbfcea0ee05672bda700e3dc18bee61ec
ksupdev <41143926+ksupdev@users.noreply.github.com>
2021-06-30 01:26:57 +0700

    Update Contract.sol
    setup zombieToOwner and count number of zombie

--- Contract.sol
+++ Contract.sol
@@ -19,8 +19,11 @@ contract ZombieFactory {
 
     function _createZombie(string memory _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+
         emit NewZombie(id, _name, _dna);
-    } 
+    }
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
         uint rand = uint(keccak256(abi.encodePacked(_str)));
